7944002518e3f756a6e255e0da686120b1974f0e
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-04 18:10:36 -0500

    test: add test for listing all discounts in Merchant Discounts Index page

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -14,14 +14,28 @@ RSpec.describe 'Merchant Discounts Index Page', type: :feature do
   it 'displays all of the discounts including their percentage discount and quantity thresholds' do 
     Faker::UniqueGenerator.clear 
     merchant_1 = Merchant.create!(name: Faker::Name.unique.name, status: 1)
+    merchant_2 = Merchant.create!(name: Faker::Name.unique.name, status: 1)
 
     discount_1a = merchant_1.discounts.create!(discount: 0.20, threshold: 10)
+    discount_1b = merchant_1.discounts.create!(discount: 0.30, threshold: 15)
 
-    binding.pry 
+    discount_2a = merchant_2.discounts.create!(discount: 0.15, threshold: 10)
+    discount_2b = merchant_2.discounts.create!(discount: 0.30, threshold: 20)
 
     visit merchant_discounts_path(merchant_1) 
 
+    within('#discount-0') do 
+      expect(page).to have_content('Discount 1')
+      expect(page).to have_content('Discount Amount: 20 percent, Threshold: 10 items')
+    end
+    
+    within('#discount-1') do 
+      expect(page).to have_content('Discount 2')
+      expect(page).to have_content('Discount Amount: 30 percent, Threshold: 15 items')
+    end
 
+    expect(page).to_not have_content('Discount Amount: 15 percent, Threshold: 10 items')
+    expect(page).to_not have_content('Discount Amount: 30 percent, Threshold: 20 items')
   end
 
   # And each bulk discount listed includes a link to its show page
