a0082d2e3348ad41ee5ea0831cee24ce9cd31adf
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-06 17:02:42 -0500

    test: add feature test for discounted revenue display in merchant invoices show page

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -58,7 +58,6 @@ RSpec.describe 'Merchant invoice Show page' do
         item_2 = Item.create!(name: 'ferbie', description: 'monster toy', unit_price: 66600, merchant_id: merchant.id)
         invoice_1 = Invoice.create!(status: 'completed', customer_id: customer.id)
 
-
         InvoiceItem.create!(quantity: 2, unit_price: 11, status: 'shipped', item: item_1, invoice: invoice_1)
         InvoiceItem.create!(quantity: 10, unit_price: 500, status: 'packaged', item: item_2, invoice: invoice_1)
 
@@ -86,6 +85,35 @@ RSpec.describe 'Merchant invoice Show page' do
             click_on("Update Item Status")
             expect(current_path).to eq("/merchants/#{merchant.id}/invoices/#{invoice_1.id}")
             expect(page).to have_content("packaged")
+        end
+    end
+
+    # US 6
+    # Merchant Invoice Show Page: Total Revenue and Discounted Revenue
+    # As a merchant
+    # When I visit my merchant invoice show page
+    # Then I see the total revenue for my merchant from this invoice (not including discounts)
+    # And I see the total discounted revenue for my merchant from this invoice which includes bulk discounts in the calculation
+    it 'shows total discounted revenue generated from all items on invoice' do
+        merchant = Merchant.create!(name: 'amazon')
+        
+        customer = Customer.create!(first_name: 'Billy', last_name: 'Bob')
+
+        item_1 = Item.create!(name: 'pet rock', description: 'a rock you pet', unit_price: 10000, merchant_id: merchant.id)
+        item_2 = Item.create!(name: 'ferbie', description: 'monster toy', unit_price: 66600, merchant_id: merchant.id)
+
+        invoice_1 = Invoice.create!(status: 'completed', customer_id: customer.id)
+
+        InvoiceItem.create!(quantity: 2, unit_price: 50, status: 'shipped', item: item_1, invoice: invoice_1)
+        InvoiceItem.create!(quantity: 15, unit_price: 100, status: 'packaged', item: item_2, invoice: invoice_1)
+
+        discount_1a = merchant_1.discounts.create!(discount: 20, threshold: 10)
+        discount_1b = merchant_1.discounts.create!(discount: 30, threshold: 15)
+
+        visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
+
+        within "#invoice-details" do
+            expect(page).to have_content("Total Discounted Revenue: $50.22")
         end
     end
 end
