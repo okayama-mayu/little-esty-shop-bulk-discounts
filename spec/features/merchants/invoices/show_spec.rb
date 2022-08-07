245ec285fec84e2c0a3363309c5c2ee175766a24
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-07 10:55:48 -0500

    adjust AdminInvoicesShow page to account for updated InoiceModel#total_revenue

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -140,7 +140,6 @@ RSpec.describe 'Merchant invoice Show page' do
         discount_1b = merchant.discounts.create!(discount: 30, threshold: 15)
 
         visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
-        save_and_open_page
 
         within "#item-details" do
             expect(page).to have_link('20.0% off Discount with Threshold of 10 Applied')
