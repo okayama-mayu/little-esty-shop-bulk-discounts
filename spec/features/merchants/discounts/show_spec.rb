520d19eb29b39e4f184312da6922adceb410eed4
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-08-09 21:13:26 -0500

    test: add feature so Discount cannot be edited when it is attached to a pending Invoice

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -41,4 +41,33 @@ RSpec.describe 'Merchant Discount Show page', type: :feature do
 
     expect(current_path).to eq "/merchants/#{merchant_1.id}/discounts/#{discount_1b.id}/edit"
   end
+
+  # Extension 1 
+  # When an invoice is pending, a merchant should not be able to delete or edit a bulk discount that applies to any of their items on that invoice.
+  it 'does not allow a Discount to be edited if the Merchant has a pending Invoice within which the Discount is applied' do 
+    Faker::UniqueGenerator.clear 
+    merchant_1 = Merchant.create!(name: Faker::Name.unique.name, status: 1)
+
+    item_1 = Item.create!(name: 'pet rock', description: 'a rock you pet', unit_price: 10000, merchant_id: merchant_1.id)
+    item_2 = Item.create!(name: 'ferbie', description: 'monster toy', unit_price: 66600, merchant_id: merchant_1.id)
+
+    discount_1a = merchant_1.discounts.create!(discount: 20, threshold: 10)
+    discount_1b = merchant_1.discounts.create!(discount: 30, threshold: 15)
+
+    customer = Customer.create!(first_name: 'Billy', last_name: 'Bob')
+
+    invoice_1 = Invoice.create!(status: 'in progress', customer_id: customer.id)
+
+    InvoiceItem.create!(quantity: 2, unit_price: 5000, status: 'pending', item: item_1, invoice: invoice_1)
+    InvoiceItem.create!(quantity: 15, unit_price: 10000, status: 'pending', item: item_2, invoice: invoice_1)
+
+    visit merchant_discount_path(merchant_1, discount_1b)
+
+    click_link 'Edit Discount'
+
+    expect(current_path).to eq "/merchants/#{merchant_1.id}/discounts/#{discount_1b.id}"
+    expect(page).to have_content 'Discount cannot be edited when an Invoice with the Discount is pending.'
+    expect(page).to have_content('Quantity Threshold: 15')
+    expect(page).to have_content('Percentage Discount: 30.0%')
+  end
 end
